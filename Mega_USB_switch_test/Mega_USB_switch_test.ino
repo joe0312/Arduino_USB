f700b8c255b8a01bf0547d7e98e7f774df9b9e4c
joe0312 <joe_shen@gemteks.com>
2015-03-16 17:01:42 +0800

    20150316 Mega_USB_switch_test(add detected console input)

--- Mega_USB_switch_test/Mega_USB_switch_test.ino
+++ Mega_USB_switch_test/Mega_USB_switch_test.ino
@@ -1,12 +1,26 @@
 //Mega USB switch test
- 	
+
+char incomingByte = 0; 
 const int conPin = 40;
 
-
 void setup() {
-  pinMode(conPin, OUTPUT); 
+  pinMode(conPin, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Connect USB (YorN)");
 }
 
 void loop() {
-  analogWrite(conPin, 255);
+  while (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    Serial.print("Connect received: ");
+    Serial.println(incomingByte);
+  }
+  
+  if (incomingByte == 'Y'){
+    analogWrite(conPin, 255);
+  }
+  else {
+    analogWrite(conPin, 0);
+  }
 }
+
