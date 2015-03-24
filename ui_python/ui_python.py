20f0c6e1793226373ce6f8c26df32ac48c1e1b1a
joe0312 <joe_shen@gemteks.com>
2015-03-24 10:10:08 +0800

    ui_python add detected com port choose(time_script.sh) detected com port(ui_python.py)

--- ui_python/ui_python.py
+++ ui_python/ui_python.py
@@ -1,12 +1,42 @@
 #!/usr/bin/env python
 
-import serial
 import sys
+import glob
+import serial
 
-ser=serial.Serial('/dev/ttyUSB0',115200)
+##### Detected com port #####
+if sys.platform.startswith('win'):
+        ports = ['COM' + str(i + 1) for i in range(256)]
 
-print("Connecting to USB ??")
-print "Input(Y or N) : ",sys.argv[1]
-ser.write(sys.argv[1])
+elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+
+else:
+        raise EnvironmentError('Unsupported platform')
+
+result = []
+
+for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+print(result[0])
 
 
+##### Transfer connecr message #####
+if sys.argv[1] == "auto":
+	ser=serial.Serial(result[0],115200)
+	print("Connecting to USB ??")
+	print "Input(Y or N) : ",sys.argv[2]
+	ser.write(sys.argv[2].encode())
+
+else:
+	ser=serial.Serial(sys.argv[1],115200)
+        print("Connecting to USB ??")
+        print "Input(Y or N) : ",sys.argv[2]
+        ser.write(sys.argv[2].encode())
+
+print(ser)
