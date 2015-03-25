7ccacb5c5e81dc6866752f06438ea38b97fc9fa8
joe0312 <joe_shen@gemteks.com>
2015-03-25 11:07:29 +0800

    20150325 time_script.sh(modified the syntax)

--- ui_python/time_script.sh
+++ ui_python/time_script.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+
 ###### Default ######
-if [ -z $1 ];then
+if [ $# -eq 0 ];then
 	devopt="auto"
+	op_mode="loop"
 	read -p "Please input USB open time(s) : " opentime
         read -p "Please input USB close time(s) : " closetime
 fi
 
 ###### Variables ####
+variable=`getopt -o d:o:c:m: -l dev:,opentime:,closetime:,mode: -- "$@"`
+eval set -- $variable
+
 while [ "$1" ]
 do
 	case $1 in
@@ -15,28 +20,44 @@ do
 		devopt=$2
 		shift 2
 		;;
-	-t|--time)
-		if [ $2 -ge 0 -a $3 -ge 0 ];then
+	-o|--opentime)
+		if [ `echo $2 | grep "^[0-9]*$"` ];then
+			op_mode="loop"
 			opentime=$2
-			closetime=$3
-			shift 3
+			shift 2
 		else
 			echo "time input error!!"
-			echo "Please input : -t opentime closetime"
+			echo "Please input : -o opentime"
                 	exit 1
 		fi
 		;;
+	-c|--closetime)
+		if [ `echo $2 | grep "^[0-9]*$"` ];then
+			op_mode="loop"
+			closetime=$2
+			shift 2
+		else
+			echo "time input error!!"
+			echo "Please input : -c closetime"
+			exit 1
+		fi
+		;;
 	-m|--mode)
-		if [ $2 == "on" -o $2 == "Y" ];then
-			opentime=1
-			closetime=0
-		elif [ $2 == "off" -o $2 == "N" ];then
-			opentime=0
-			closetime=1
+		if [ $2 = "on" -o $2 = "Y" ];then
+			op_mode="single"
+			switchopt="Y"
+		elif [ $2 = "off" -o $2 = "N" ];then
+			op_mode="single"
+			switchopt="N"
 		else
 			echo "mode input error!!"
+			exit 1
 		fi
 		shift 2
+		;;
+	--)
+		shift
+		break
 		;;
 	*)
 		echo "input format error!!"
@@ -46,7 +67,7 @@ do
 done
 
 ##### Operating mode #####
-if [ $opentime \> 0 -a $closetime \> 0 ];then
+if [ "$op_mode" = "loop" ];then
 	while :
 	do
 		sleep $opentime
@@ -54,9 +75,7 @@ if [ $opentime \> 0 -a $closetime \> 0 ];then
 		sleep $closetime
 		python ui_python.py $devopt N
 	done
-elif [ $closetime \> 0 ];then
-	python ui_python.py $devopt N
-elif [ $opentime \> 0 ];then
-	python ui_python.py $devopt Y
+elif [ "$op_mode" = "single" ];then
+	python ui_python.py $devopt $switchopt
 fi
 
